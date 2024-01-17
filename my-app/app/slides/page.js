ba152d24b9e0f3577da468b5f86251ed4b55c66a
Gavgastyolo <gavinmeijer@gmail.com>
2024-01-17 13:05:25 +0100

    CARROUSEL

--- my-app/app/slides/page.js
+++ my-app/app/slides/page.js
@@ -1,16 +1,27 @@
 "use client";
-import React from 'react'
-import Ns from './Ns'
-import Weather from './Weather'
-import Welcome from './Welcome'
+import React, { useEffect, useState } from "react";
+import Ns from "./Ns";
+import Weather from "./Weather";
+import Welcome from "./Welcome";
 
+export default function Home() {
+  const [tresh, setTresh] = useState(0);
+  const time = 10000;
 
-export default function Home () {
-    return (
-        <>
-            <Ns />
-            <Weather />
-            <Welcome />
-        </>
-    );
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTresh((prevTresh) => (prevTresh === 0 ? 1 : 0));
+    }, 5000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      {tresh === 0 && <Ns />}
+      {tresh === 1 && <Weather />}
+    </>
+  );
 }
+    
