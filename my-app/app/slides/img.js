114402bc507276d5ddbf2512a7c511cf6ca68b90
Gavgastyolo <gavinmeijer@gmail.com>
2024-02-01 09:59:53 +0100

    SOS

--- my-app/app/slides/img.js
+++ my-app/app/slides/img.js
@@ -4,8 +4,12 @@ export const Img = ({ image }) => {
   console.log(image);
   const imageUrl = image.url[0].url;
   return (
-    <div>
-      <img src={imageUrl} alt={`slide ${image.id}`} />
+    <div className="flex w-[100vw] h-[90vh] items-center justify-center">
+      <img
+        className="min-w-[auto] min-h-[100vh]"
+        src={imageUrl}
+        alt={`slide ${image.id}`}
+      />
     </div>
   );
 };
